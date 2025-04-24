1de1054cb9bef9f23c796ba986d2aa60dd039145
jamesfelicitas <jamesivanfelicitas@gmail.com>
2025-04-25 01:18:18 +0800

    Initial setup for EditAccount component

---
+++ src/pages/EditProfilePage.tsx
@@ -0,0 +1,10 @@
+import React, { useState, useRef, useEffect } from 'react';
+import {
+  IonContent, IonPage, IonInput, IonButton, IonAlert, IonHeader,
+  IonBackButton, IonButtons, IonItem, IonText, IonCol, IonGrid,
+  IonRow, IonInputPasswordToggle, IonImg, IonAvatar,
+} from '@ionic/react';
+import { supabase } from '../utils/supabaseClient';
+import { useHistory } from 'react-router-dom';
+
+const EditAccount: React.FC = () => {
