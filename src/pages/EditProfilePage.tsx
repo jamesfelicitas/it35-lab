370b3bbc231d560c4e57255c27ad09db43c8b0a3
jamesfelicitas <jamesivanfelicitas@gmail.com>
2025-04-25 01:19:23 +0800

    Add useState and useRef hooks

--- src/pages/EditProfilePage.tsx
+++ src/pages/EditProfilePage.tsx
@@ -8,3 +8,17 @@ import { supabase } from '../utils/supabaseClient';
 import { useHistory } from 'react-router-dom';
 
 const EditAccount: React.FC = () => {
+    const [email, setEmail] = useState('');
+    const [currentPassword, setCurrentPassword] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [username, setUsername] = useState('');
+    const [avatarFile, setAvatarFile] = useState<File | null>(null);
+    const [avatarPreview, setAvatarPreview] = useState<string | null>(null);
+    const [showAlert, setShowAlert] = useState(false);
+    const [alertMessage, setAlertMessage] = useState('');
+    const history = useHistory();
+    const fileInputRef = useRef<HTMLInputElement>(null);
+  
