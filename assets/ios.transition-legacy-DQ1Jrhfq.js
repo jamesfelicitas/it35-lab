999625468a59f8271b3b25574439ea8bce24827a
jamesfelicitas <jamesivanfelicitas@gmail.com>
2025-05-14 23:02:38 +0800

    Updates

--- assets/ios.transition-legacy-DQ1Jrhfq.js
+++ assets/ios.transition-legacy-DQ1Jrhfq.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Cu0uQwLV.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-_SObYV6M.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-11xLLcoG.js
+++ assets/swipe-back-legacy-11xLLcoG.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Cu0uQwLV.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-_SObYV6M.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
